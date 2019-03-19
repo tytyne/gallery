8bb71818ce0bb87bbbe8f93f3d5b7a99b4534294
tytyne <dusaflora2@gmail.com>
2019-03-19 20:03:28 +0200

    add location dropdown menus

--- photos/urls.py
+++ photos/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     url(r'^$',views.article,name='photosToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
     url(r'^search/', views.search_category, name='search_category'),  
-    url(r'^location/', views.display_location, name='display_location'), 
+    url(r'^^media\/', views.display_location, name='display_location'), 
     url(r'^article/(\d+)',views.article,name ='article')
  
 ]
